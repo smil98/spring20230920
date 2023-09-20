977b83c465ea38b4ac8c250016e0ff1db621c696
smil98 <seungmilee98@gmail.com>
2023-09-20 16:20:01 +0900

    Controller

---
+++ src/main/java/com/example/spring20230920/controller/Controller01.java
@@ -0,0 +1,22 @@
+package com.example.spring20230920.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Controller01 {
+    @RequestMapping("/") //derived from the header. Since we don't know who requested stuff behind / is omitted
+    public void method1() {
+        System.out.println("Controller01.method1");
+    }
+
+    @RequestMapping("/path1")
+    public void method2() {
+        System.out.println("Controller01.method2");
+    }
+
+    @RequestMapping("/path2")
+    public void method3() {
+        System.out.println("Controller01.method3");
+    }
+}
