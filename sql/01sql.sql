3d6ed4b4a752395566a54ed5018ca9b5b9eda8b5
smil98 <seungmilee98@gmail.com>
2023-10-04 11:10:40 +0900

    SQL- Select

---
+++ sql/01sql.sql
@@ -0,0 +1,12 @@
+-- ctrl + enter to process
+# can also use this
+
+select * from w3schools.customers;
+select * from w3schools.employees;
+select *
+from w3schools.customers;
+-- sql is case insensitive
+
+SELECT * FROM W3SCHOOLS.CUSTOMers;
+
+-- common syntax = uppercase for keyword
---
+++ sql/02select.sql
@@ -0,0 +1,64 @@
+-- SELECT : reads data
+
+SELECT * FROM w3schools.customers;
+SELECT * FROM w3schools.employees;
+SELECT * FROM w3schools.products;
+SELECT CustomerID, CustomerName
+FROM w3schools.customers
+WHERE CustomerName = 'Alfreds Futterkiste';
+
+
+SELECT * FROM customers;
+SELECT * FROM categories;
+SELECT * FROM orderdetails;
+SELECT * FROM orders;
+SELECT * FROM products;
+SELECT * FROM shippers;
+SELECT * FROM suppliers;
+
+-- after select, col name
+-- after from, table name
+
+SELECT CustomerName
+FROM customers;
+
+SELECT CustomerID, CustomerName
+FROM customers;
+
+SELECT CustomerID, CustomerName, Country -- shows up in order listed
+FROM customers;
+
+SELECT CustomerName, Country, CustomerID
+FROM customers;
+
+-- Practice
+
+SELECT LastName, FirstName, BirthDate
+FROM employees;
+
+SELECT ProductName, Unit, Price
+FROM products;
+
+SELECT SupplierName, Address, Country
+FROM suppliers;
+
+-- this allows duplicate data show up
+SELECT Country from customers;
+
+-- to only select unique data, use DISTINCT
+SELECT DISTINCT Country from customers;
+
+SELECT City, Country FROM customers;
+SELECT DISTINCT City, Country FROM customers;
+
+-- COUNT : counts tuples
+
+SELECT COUNT(Country) FROM customers; -- 91
+SELECT COUNT(DISTINCT Country) FROM customers; -- 21
+
+SELECT COUNT(*) FROM customers; -- entire tuple = 91
+
+SELECT products.ProductID, products.ProductName, suppliers.SupplierName
+FROM products
+INNER JOIN suppliers
+ON products.SupplierID = suppliers.SupplierID;
