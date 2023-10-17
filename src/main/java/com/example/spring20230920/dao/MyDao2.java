8c4d10e8dfc39793602d12d3d6b2c6719bc5747f
smil98 <seungmilee98@gmail.com>
2023-10-17 16:31:54 +0900

    Spring: MyBatis

---
+++ src/main/java/com/example/spring20230920/dao/MyDao2.java
@@ -0,0 +1,15 @@
+package com.example.spring20230920.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface MyDao2 {
+
+    @Select("""
+    SELECT CONCAT(FirstName, ' ', LastName) Name
+    FROM employees
+    WHERE EmployeeID = #{id}
+    """)
+    String select(Integer id);
+}
