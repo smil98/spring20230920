963283e739b093c5338246558a467fa565ea5c04
smil98 <seungmilee98@gmail.com>
2023-11-08 12:31:41 +0900

    RestController = @Controller + @ResponseBody

--- src/main/java/com/example/spring20230920/controller/RestController1.java
+++ src/main/java/com/example/spring20230920/controller/RestController1.java
@@ -7,10 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
-@Controller
+@RestController //@Controller + @ResponseBody
 @RequiredArgsConstructor
 @RequestMapping("api/main1")
 public class RestController1 {
